5e7134a1005a15da5aebe1ad1508cabdf03b43c1
BestZige <bestzige@hotmail.com>
2023-12-18 21:24:45 +0700

    Fix sql file error

--- mysql.sql
+++ mysql.sql
@@ -1,36 +1,9 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema simple-rbac-java-servlet
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema simple-rbac-java-servlet
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `simple-rbac-java-servlet` DEFAULT CHARACTER SET utf8mb4 ;
 USE `simple-rbac-java-servlet` ;
 
--- -----------------------------------------------------
--- Table `simple-rbac-java-servlet`.`users`
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `simple-rbac-java-servlet`.`users` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(255) NULL DEFAULT NULL,
-  `password` VARCHAR(255) NULL DEFAULT NULL,
-  `role` VARCHAR(50) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `username` (`username` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+    role VARCHAR(50)
+);
